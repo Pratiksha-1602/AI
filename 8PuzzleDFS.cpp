a373b86f8a75d7403dca9ff4d77bce6cc437d588
Pratiksha-1602 <109472126+Pratiksha-1602@users.noreply.github.com>
2024-05-26 21:02:56 +0530

    Update 8PuzzleDFS.cpp

--- 8PuzzleDFS.cpp
+++ 8PuzzleDFS.cpp
@@ -1,32 +1,126 @@
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+
+
+// bool checkEqual(vector<vector<int>> curr, vector<vector<int>> final){
+//     for(int i = 0; i < 3; i++){
+//         for(int j = 0; j < 3; j++){
+//             if(curr[i][j] != final[i][j])
+//                 return false;
+//         }
+//     }
+//     return true;
+// }
+
+// pair<int, int> findBlank(vector<vector<int>> curr){
+//     pair<int, int> blank;
+//     for(int i = 0; i < 3; i++){
+//         for(int j = 0; j < 3; j++){
+//             if(curr[i][j] == 0)
+//             blank = {i, j};
+//         }
+//     }
+//     return blank;
+// }
+
+// void printAns(vector<vector<vector<int>>> vec){
+//     for(auto mat : vec){
+//         for(auto row : mat){
+//             for(auto ele : row){
+//                 cout << ele << " ";
+//             }
+//             cout << endl;
+//         }
+//         cout << endl;
+//     }
+//     cout << endl;
+// }
+
+// bool dfs(vector<vector<int>>& curr, vector<vector<int>>& final, int depth, int row, int col, set<vector<vector<int>>>& vis, vector<vector<vector<int>>>& vec){
+//     if(depth > 10)
+//         return false;
+
+//     vec.push_back(curr);
+//     vis.insert(curr);
+
+//     if(checkEqual(curr, final)){
+//         cout << "The possible solution is : " << endl;
+//         printAns(vec);
+//         return true;
+//     }
+
+//     int delRow[] = {0, 1, 0, -1};
+//     int delCol[] = {1, 0, -1, 0};
+
+//     for(int i = 0; i < 4; i++){
+//         int nRow = row + delRow[i];
+//         int nCol = col + delCol[i];
+
+//         if(nRow >= 0 && nRow < 3 && nCol >= 0 && nCol < 3){
+//             swap(curr[row][col], curr[nRow][nCol]);
+//             if(!vis.count(curr)){
+//                 if(dfs(curr, final, depth + 1, nRow, nCol, vis, vec))
+//                     return true;
+//             }
+//             swap(curr[nRow][nCol], curr[row][col]);
+//         }
+//     }
+//     vec.pop_back();
+
+//     return false;
+// }
+
+// int main(){
+
+//     vector<vector<int>> start = {{2, 8, 3},
+//                                   {1, 6, 4},
+//                                   {7, 0, 5}};
+//     vector<vector<int>> final = {{1, 2, 3},
+//                                   {8, 0, 4},
+//                                   {7, 6, 5}};
+
+//     pair<int, int> blank = findBlank(start);
+//     int row = blank.first;
+//     int col = blank.second;
+
+//     vector<vector<vector<int>>> ans;
+//     set<vector<vector<int>>> vis;
+
+//     if(!dfs(start, final, 0, row, col, vis, ans))
+//         cout << "No Solution found" << endl;
+
+//     return 0;
+
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
 
-
-bool checkEqual(vector<vector<int>> curr, vector<vector<int>> final){
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            if(curr[i][j] != final[i][j])
+bool checkEqual(const vector<vector<int>>& curr, const vector<vector<int>>& final) {
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (curr[i][j] != final[i][j])
                 return false;
         }
     }
     return true;
 }
 
-pair<int, int> findBlank(vector<vector<int>> curr){
+pair<int, int> findBlank(const vector<vector<int>>& curr) {
     pair<int, int> blank;
-    for(int i = 0; i < 3; i++){
-        for(int j = 0; j < 3; j++){
-            if(curr[i][j] == 0)
-            blank = {i, j};
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (curr[i][j] == 0)
+                blank = {i, j};
         }
     }
     return blank;
 }
 
-void printAns(vector<vector<vector<int>>> vec){
-    for(auto mat : vec){
-        for(auto row : mat){
-            for(auto ele : row){
+void printAns(const vector<vector<vector<int>>>& vec) {
+    for (const auto& mat : vec) {
+        for (const auto& row : mat) {
+            for (const auto& ele : row) {
                 cout << ele << " ";
             }
             cout << endl;
@@ -36,14 +130,14 @@ void printAns(vector<vector<vector<int>>> vec){
     cout << endl;
 }
 
-bool dfs(vector<vector<int>>& curr, vector<vector<int>>& final, int depth, int row, int col, set<vector<vector<int>>>& vis, vector<vector<vector<int>>>& vec){
-    if(depth > 10)
+bool depthLimitedSearch(vector<vector<int>>& curr, const vector<vector<int>>& final, int depth, int limit, int row, int col, set<vector<vector<int>>>& vis, vector<vector<vector<int>>>& vec) {
+    if (depth > limit)
         return false;
 
     vec.push_back(curr);
     vis.insert(curr);
 
-    if(checkEqual(curr, final)){
+    if (checkEqual(curr, final)) {
         cout << "The possible solution is : " << endl;
         printAns(vec);
         return true;
@@ -52,14 +146,14 @@ bool dfs(vector<vector<int>>& curr, vector<vector<int>>& final, int depth, int r
     int delRow[] = {0, 1, 0, -1};
     int delCol[] = {1, 0, -1, 0};
 
-    for(int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; ++i) {
         int nRow = row + delRow[i];
         int nCol = col + delCol[i];
 
-        if(nRow >= 0 && nRow < 3 && nCol >= 0 && nCol < 3){
+        if (nRow >= 0 && nRow < 3 && nCol >= 0 && nCol < 3) {
             swap(curr[row][col], curr[nRow][nCol]);
-            if(!vis.count(curr)){
-                if(dfs(curr, final, depth + 1, nRow, nCol, vis, vec))
+            if (!vis.count(curr)) {
+                if (depthLimitedSearch(curr, final, depth + 1, limit, nRow, nCol, vis, vec))
                     return true;
             }
             swap(curr[nRow][nCol], curr[row][col]);
@@ -70,14 +164,13 @@ bool dfs(vector<vector<int>>& curr, vector<vector<int>>& final, int depth, int r
     return false;
 }
 
-int main(){
-
+int main() {
     vector<vector<int>> start = {{2, 8, 3},
-                                  {1, 6, 4},
-                                  {7, 0, 5}};
+                                 {1, 6, 4},
+                                 {7, 0, 5}};
     vector<vector<int>> final = {{1, 2, 3},
-                                  {8, 0, 4},
-                                  {7, 6, 5}};
+                                 {8, 0, 4},
+                                 {7, 6, 5}};
 
     pair<int, int> blank = findBlank(start);
     int row = blank.first;
@@ -85,10 +178,10 @@ int main(){
 
     vector<vector<vector<int>>> ans;
     set<vector<vector<int>>> vis;
+    int depthLimit = 10;  // Define the depth limit here
 
-    if(!dfs(start, final, 0, row, col, vis, ans))
-        cout << "No Solution found" << endl;
+    if (!depthLimitedSearch(start, final, 0, depthLimit, row, col, vis, ans))
+        cout << "No Solution found within depth limit" << endl;
 
     return 0;
-
 }
